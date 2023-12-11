439472a1bb8b3d6836dc53ecfbb23e6235427afb
Abu-Bakar-Siddik-Arafat <abu15-13809@diu.edu.bd>
2023-12-11 23:28:38 +0600

    live test

--- Assignment&Live Test/Module 5/Live Test/liveTest.dart
+++ Assignment&Live Test/Module 5/Live Test/liveTest.dart
@@ -4,12 +4,9 @@ void main() {
     {"name": "Banana", "color": "Yellow", "price": 1.0},
     {"name": "Grapes", "color": "Purple", "price": 3.0},
   ];
-
   print("Original Fruit Details before Discount:");
   displayFruitDetails(fruits);
-
   applyPriceDiscount(fruits, 10);
-
   print("\nFruit Details After Applying 10% Discount:");
   displayFruitDetails(fruits);
 }
